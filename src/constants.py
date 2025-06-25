0fb2fb9c64bb63febfd14669a6fad096d1e4d533
Dario Gutierrez <dariofg93@gmail.com>
2025-06-25 19:21:10 -0300

    Change db location

--- src/constants.py
+++ src/constants.py
@@ -4,4 +4,4 @@ from pathlib import Path
 DEFAULT_COLOR_TEMP: int = 3000
 DEFAULT_BRIGHTNESS: int = 128
 
-DATABASE_PATH: Path = Path(__file__).resolve().parent / "data" / "wiz_controller.db"
+DATABASE_PATH: Path = Path(__file__).resolve().parent.parent / "data" / "wiz_controller.db"
