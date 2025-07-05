d9789f1d349c3869948c3f95077d000041176cf8
Dario Gutierrez <dariofg93@gmail.com>
2025-07-05 20:10:35 -0300

    Add rpi.gpio library and test code

---
+++ src/prompt/rpigpio_test.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+botones = {
+    17: "Botón Rojo",
+    27: "Botón Azul",
+    22: "Botón Verde"
+}
+
+for pin in botones:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+print("Esperando pulsación...")
+try:
+    while True:
+        for pin, nombre in botones.items():
+            if GPIO.input(pin) == GPIO.LOW:
+                print(f"nombre: {nombre}")
+                print(f"pin: {pin}")
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
