8ee7bfd112f4789c0a3e88949d73f8e444847921
Dario Gutierrez <dariofg93@gmail.com>
2025-07-06 20:32:14 -0300

    test

--- src/prompt/rpigpio_test.py
+++ src/prompt/rpigpio_test.py
@@ -10,12 +10,14 @@ botones = {
 GPIO.setmode(GPIO.BCM)  # <-- esta línea es necesaria
 
 for pin in botones:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 print("Esperando pulsación...")
 try:
     while True:
         for pin, nombre in botones.items():
+            print(f"nombre: {nombre}")
+            print(f"pin: {pin}")
             if GPIO.input(pin) == GPIO.LOW:
                 print(f"nombre: {nombre}")
                 print(f"pin: {pin}")
