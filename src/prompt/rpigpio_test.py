c3410d5d00643ff3046b7d92fea90042049ada7e
Dario Gutierrez <dariofg93@gmail.com>
2025-07-06 20:39:54 -0300

    test

--- src/prompt/rpigpio_test.py
+++ src/prompt/rpigpio_test.py
@@ -7,18 +7,31 @@ botones = {
     3: "Botón Verde"
 }
 
-GPIO.setmode(GPIO.BCM)  # <-- esta línea es necesaria
+GPIO.setmode(GPIO.BOARD)  # <-- esta línea es necesaria
 
-for pin in botones:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(17, GPIO.IN)
+GPIO.setup(3, GPIO.IN)
+GPIO.setup(1, GPIO.IN)
 
-print("Esperando pulsación...")
-try:
-    while True:
-        for pin, nombre in botones.items():
-            if GPIO.input(pin) == GPIO.LOW:
-                print(f"nombre: {nombre}")
-                print(f"pin: {pin}")
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+if GPIO.input(17) == 0:
+    print("presionado 17")
+
+if GPIO.input(3) == 0:
+    print("presionado 3")
+
+if GPIO.input(1) == 0:
+    print("presionado 1")
+
+# for pin in botones:
+#     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# print("Esperando pulsación...")
+# try:
+#     while True:
+#         for pin, nombre in botones.items():
+#             if GPIO.input(pin) == GPIO.LOW:
+#                 print(f"nombre: {nombre}")
+#                 print(f"pin: {pin}")
+#         time.sleep(0.1)
+# except KeyboardInterrupt:
+#     GPIO.cleanup()
