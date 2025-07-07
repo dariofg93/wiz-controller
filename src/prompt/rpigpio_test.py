8628053d94f97d609dc001a0809d7e676778503f
Dario Gutierrez <dariofg93@gmail.com>
2025-07-07 16:28:26 -0300

    test

--- src/prompt/rpigpio_test.py
+++ src/prompt/rpigpio_test.py
@@ -4,8 +4,8 @@ import time
 GPIO.setmode(GPIO.BCM)  # <-- esta línea es necesaria
 print(GPIO.RPI_INFO)
 
-print(GPIO.input(2))
-print(GPIO.input(3))
+GPIO.setup(2, GPIO.IN)
+GPIO.setup(3, GPIO.IN)
 # GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # GPIO.setup(1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # print(GPIO.input(17))
