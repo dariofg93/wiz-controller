53d069b7dc8e94ec24a8a5303a8b49b87507f7d5
Dario Gutierrez <dariofg93@gmail.com>
2025-07-09 19:56:43 -0300

    test

--- src/prompt/test.py
+++ src/prompt/test.py
@@ -4,23 +4,19 @@ import time
 PIN_BOTON_AC_2 = 2
 PIN_BOTON_AC_3 = 3
 
-# `chip` es 0 por defecto en Raspberry Pi
-CHIP = 0
+# Abrir chip 0
+h = lgpio.gpiochip_open(0)
+
+# Reclamar los pines como entrada con pull-up
+lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_2)
+lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_3)
 
 try:
     print("Esperando inputs... presiona el botón en el Pin 2 o Pin 3.")
 
-    # Configurar pines
-    lgpio.set_mode(CHIP, PIN_BOTON_AC_2, lgpio.INPUT)
-    lgpio.set_mode(CHIP, PIN_BOTON_AC_3, lgpio.INPUT)
-
-    # Activar pull-up interno
-    lgpio.set_pull_up_down(CHIP, PIN_BOTON_AC_2, lgpio.PUD_UP)
-    lgpio.set_pull_up_down(CHIP, PIN_BOTON_AC_3, lgpio.PUD_UP)
-
     while True:
-        estado_pin_2 = lgpio.read(CHIP, PIN_BOTON_AC_2)
-        estado_pin_3 = lgpio.read(CHIP, PIN_BOTON_AC_3)
+        estado_pin_2 = lgpio.gpio_read(h, PIN_BOTON_AC_2)
+        estado_pin_3 = lgpio.gpio_read(h, PIN_BOTON_AC_3)
 
         if estado_pin_2 == 0:
             print("¡Pin 2 presionado!")
@@ -36,4 +32,5 @@ except Exception as e:
     print(f"Ocurrió un error inesperado: {e}")
 
 finally:
+    lgpio.gpiochip_close(h)
     print("Script finalizado.")
