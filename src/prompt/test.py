ae5a591c9ffecb55a12d1c30417a47d66f063714
Dario Gutierrez <dariofg93@gmail.com>
2025-07-09 19:12:16 -0300

    test

--- src/prompt/test.py
+++ src/prompt/test.py
@@ -1,30 +1,45 @@
-from lgpio import Gpio
-#try:
-#    from rpi_lgpio import Gpio
-#except ImportError:
-#    from src.mock.mock_gpio import Gpio
-
+import lgpio
 import signal
+
+class GpioWrapper:
+    def __init__(self):
+        # Abre el chip GPIO 0 (el default en Raspberry Pi)
+        self.h = lgpio.gpiochip_open(0)
+
+    def set_pull_up_down(self, pin, pud):
+        # Configura pull-up o pull-down
+        # En lgpio, pud es: LGPIO_PULL_UP, LGPIO_PULL_DOWN, o LGPIO_PULL_NONE
+        lgpio.gpio_set_pull_up_down(self.h, pin, pud)
+
+    def callback(self, pin, edge, func):
+        # edge puede ser: LGPIO_BOTH_EDGES, LGPIO_RISING_EDGE, LGPIO_FALLING_EDGE
+        return lgpio.callback(self.h, pin, edge, func)
+
+    def close(self):
+        lgpio.gpiochip_close(self.h)
+
+# Constantes de lgpio
+PUD_UP = lgpio.LGPIO_PULL_UP
+FALLING_EDGE = lgpio.LGPIO_FALLING_EDGE
 
 button_pins = [2, 3]
 
-# Creamos una instancia del controlador
-gpio = Gpio()
+# Creamos la instancia
+gpio = GpioWrapper()
 
-# Configuramos los pines como entrada con pull-up
+# Configuramos los pines con pull-up
 for pin in button_pins:
-    gpio.set_mode(pin, Gpio.INPUT)
-    gpio.set_pull_up_down(pin, Gpio.PUD_UP)
+    gpio.set_pull_up_down(pin, PUD_UP)
 
-# Callback que se llama cuando se detecta un flanco de bajada
+# Callback para flanco de bajada
 def button_callback(pin, level, tick):
     print(f"Botón en pin {pin} presionado (level={level}, tick={tick})")
 
-# Registramos el callback para cada pin
+# Registramos callback para cada pin
 for pin in button_pins:
-    gpio.callback(pin, Gpio.FALLING_EDGE, button_callback)
+    gpio.callback(pin, FALLING_EDGE, button_callback)
 
-print("Esperando que se presionen botones. Presioná Ctrl+C para salir.")
+print("Esperando que se presionen botones. Ctrl+C para salir.")
 
 try:
     signal.pause()
