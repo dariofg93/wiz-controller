2743f49f67823386de962de53385eab9f13a845b
Dario Gutierrez <dariofg93@gmail.com>
2025-07-10 17:31:55 -0300

    test

--- src/prompt/test.py
+++ src/prompt/test.py
@@ -1,22 +1,22 @@
 import lgpio
 import time
 
-PIN_BOTON_AC_2 = 2
-PIN_BOTON_AC_3 = 3
+PIN_BOTON_AC_4 = 4
+PIN_BOTON_AC_18 = 18
 
 # Abrir chip 0
 h = lgpio.gpiochip_open(0)
 
 # Reclamar los pines como entrada con pull-up
-lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_2)
-lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_3)
+lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_4)
+lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_18)
 
 try:
     print("Esperando inputs... presiona el botón en el Pin 2 o Pin 3.")
 
     while True:
-        estado_pin_2 = lgpio.gpio_read(h, PIN_BOTON_AC_2)
-        estado_pin_3 = lgpio.gpio_read(h, PIN_BOTON_AC_3)
+        estado_pin_2 = lgpio.gpio_read(h, PIN_BOTON_AC_4)
+        estado_pin_3 = lgpio.gpio_read(h, PIN_BOTON_AC_18)
 
         if estado_pin_2 == 0:
             print("¡Pin 2 presionado!")
