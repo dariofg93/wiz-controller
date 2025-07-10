053ff883f401642fdb00331cc4e44341abade8c9
Dario Gutierrez <dariofg93@gmail.com>
2025-07-10 17:41:06 -0300

    test

--- src/prompt/test.py
+++ src/prompt/test.py
@@ -1,36 +1,29 @@
-import lgpio
+import gpiod
 import time
 
-PIN_BOTON_AC_4 = 4
-PIN_BOTON_AC_18 = 18
+# Abrir chip0 (pinctrl-rp1)
+chip = gpiod.Chip('gpiochip0')
 
-# Abrir chip 0
-h = lgpio.gpiochip_open(0)
+# Pines a usar
+pins = [4, 5]  # cambiá por los que quieras
 
-# Reclamar los pines como entrada con pull-up
-lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_4)
-lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_18)
+# Pedir como entrada con pull-up
+lines = chip.get_lines(pins)
+lines.request(consumer='test', type=gpiod.LINE_REQ_DIR_IN, flags=gpiod.LINE_REQ_FLAG_BIAS_PULL_UP)
 
 try:
-    print("Esperando inputs... presiona el botón en el Pin 4 o Pin 18.")
-
+    print("Esperando botones en GPIO 4 o GPIO 5...")
     while True:
-        estado_pin_4 = lgpio.gpio_read(h, PIN_BOTON_AC_4)
-        estado_pin_18 = lgpio.gpio_read(h, PIN_BOTON_AC_18)
-
-        if estado_pin_4 == 0:
-            print("¡Pin 4 presionado!")
+        values = lines.get_values()
+        if values[0] == 0:
+            print("¡Botón en GPIO 4 presionado!")
             time.sleep(0.5)
-
-        if estado_pin_18 == 0:
-            print("¡Pin 18 presionado!")
+        if values[1] == 0:
+            print("¡Botón en GPIO 5 presionado!")
             time.sleep(0.5)
-
         time.sleep(0.1)
-
-except Exception as e:
-    print(f"Ocurrió un error inesperado: {e}")
-
+except KeyboardInterrupt:
+    print("Salida con Ctrl+C")
 finally:
-    lgpio.gpiochip_close(h)
-    print("Script finalizado.")
+    lines.release()
+    chip.close()
