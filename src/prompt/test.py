bf577ae5db8c328fc0e4b679f65115ac86cc3889
Dario Gutierrez <dariofg93@gmail.com>
2025-07-10 17:33:02 -0300

    test

--- src/prompt/test.py
+++ src/prompt/test.py
@@ -12,18 +12,18 @@ lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_4)
 lgpio.gpio_claim_input(h, lgpio.SET_PULL_UP, PIN_BOTON_AC_18)
 
 try:
-    print("Esperando inputs... presiona el botón en el Pin 2 o Pin 3.")
+    print("Esperando inputs... presiona el botón en el Pin 4 o Pin 18.")
 
     while True:
-        estado_pin_2 = lgpio.gpio_read(h, PIN_BOTON_AC_4)
-        estado_pin_3 = lgpio.gpio_read(h, PIN_BOTON_AC_18)
+        estado_pin_4 = lgpio.gpio_read(h, PIN_BOTON_AC_4)
+        estado_pin_18 = lgpio.gpio_read(h, PIN_BOTON_AC_18)
 
-        if estado_pin_2 == 0:
-            print("¡Pin 2 presionado!")
+        if estado_pin_4 == 0:
+            print("¡Pin 4 presionado!")
             time.sleep(0.5)
 
-        if estado_pin_3 == 0:
-            print("¡Pin 3 presionado!")
+        if estado_pin_18 == 0:
+            print("¡Pin 18 presionado!")
             time.sleep(0.5)
 
         time.sleep(0.1)
